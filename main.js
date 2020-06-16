d5fdbc98beef1d234bfee4ac7761371e191802b1
Marcin Kaz <mrad19830@gmail.com>
2020-06-16 17:48:43 +0200

    Modify html from JS

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 const name = "Marcin";
 const age = 37;
 console.log("Dzień dobry " + name + age);
+
+const heading = document.querySelector('.navigation-site__link');
+console.log(heading);
+
+heading.innerHTML = 'Test JS';
